a643028f4b76532a1c1821c441d3aa6e9fa0c67f
Himanshu Gupta <himanshugupta@Himanshus-MacBook-Air.local>
2023-06-22 10:04:18 +0530

    small-change

--- db.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const password = '1234567890';
-return mongoose.connect(
-  `mongodb+srv://hgupta2363:${password}@cluster0.v0mghpu.mongodb.net/?retryWrites=true&w=majority`,
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
